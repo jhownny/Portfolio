f0140022ab02fdc0b7ce9e745c56337fb349a575
Jhonata Batista <88687538+jhownny@users.noreply.github.com>
2023-09-20 18:51:43 +0000

    Adição de var javascript e personalização de ponteiro - 20/09 Codespace

--- script/script.js
+++ script/script.js
@@ -1,77 +1,85 @@
 /* ----- NAVIGATION BAR FUNCTION ----- */
 function myMenuFunction(){
-    let menuBtn = document.getElementById("myNavMenu");
+  let menuBtn = document.getElementById("myNavMenu");
 
-    if(menuBtn.className === "nav-menu"){
-      menuBtn.className += " responsive";
-    } else {
-      menuBtn.className = "nav-menu";
+  if(menuBtn.className === "nav-menu"){
+    menuBtn.className += " responsive";
+  } else {
+    menuBtn.className = "nav-menu";
+  }
+}
+
+/* ----- DARK AND LIGHT MODE ----- */
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+  
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
     }
-  }
+}
 
-  /* ----- DARK AND LIGHT MODE ----- */
-  const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-  const currentTheme = localStorage.getItem('theme');
-  
-  if (currentTheme) {
-      document.documentElement.setAttribute('data-theme', currentTheme);
-    
-      if (currentTheme === 'dark') {
-          toggleSwitch.checked = true;
-      }
-  }
-  
-  function switchTheme(e) {
-      if (e.target.checked) {
-          document.documentElement.setAttribute('data-theme', 'dark');
-          localStorage.setItem('theme', 'dark');
-      }
-      else {        document.documentElement.setAttribute('data-theme', 'light');
-            localStorage.setItem('theme', 'light');
-      }    
-  }
-  
-  toggleSwitch.addEventListener('change', switchTheme, false);
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {        document.documentElement.setAttribute('data-theme', 'light');
+          localStorage.setItem('theme', 'light');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
 
 
 /* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
-  window.onscroll = function() {headerShadow()};
+window.onscroll = function() {headerShadow()};
 
-  function headerShadow() {
-    const navHeader =document.getElementById("header");
+function headerShadow() {
+  const navHeader =document.getElementById("header");
 
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
 
-      navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
-      navHeader.style.height = "70px";
-      navHeader.style.lineHeight = "70px";
+    navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+    navHeader.style.height = "70px";
+    navHeader.style.lineHeight = "70px";
 
-    } else {
+  } else {
 
-      navHeader.style.boxShadow = "none";
-      navHeader.style.height = "90px";
-      navHeader.style.lineHeight = "90px";
+    navHeader.style.boxShadow = "none";
+    navHeader.style.height = "90px";
+    navHeader.style.lineHeight = "90px";
 
-    }
   }
+}
 
 
 /* ----- TYPING EFFECT ----- */
- let typingEffect = new Typed(".typedText",{
-    strings : ['String nome = "Jhonata Batista";','nome = str("Jhonata Batista")','$nome = "Jhonata Batista";'],
-    loop : true,
-    typeSpeed : 100, 
-    backSpeed : 100,
-    backDelay : 200
- })
+let typingEffect = new Typed(".typedText",{
+  strings : ['String nome = "Jhonata Batista";','nome = str("Jhonata Batista")','$nome = "Jhonata Batista";'],
+  loop : true,
+  typeSpeed : 100, 
+  backSpeed : 100,
+  backDelay : 200
+})
+
+let nameTypingEffect  = new Typed(".typedTextName", {
+   strings : ['Jhownny'],
+  loop : false,
+  typeSpeed : 200, 
+  backSpeed : 100,
+  backDelay : 200
+})
 
 /* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
- const sr = ScrollReveal({
-        origin: 'top',
-        distance: '80px',
-        duration: 2000,
-        reset: true     
- })
+const sr = ScrollReveal({
+      origin: 'top',
+      distance: '80px',
+      duration: 2000,
+      reset: true     
+})
 
 /* -- HOME -- */
 sr.reveal('.featured-text-card',{})
@@ -92,10 +100,10 @@ sr.reveal('.top-header',{})
 
 /* -- ABOUT INFO & CONTACT INFO -- */
 const srLeft = ScrollReveal({
-  origin: 'left',
-  distance: '80px',
-  duration: 2000,
-  reset: true
+origin: 'left',
+distance: '80px',
+duration: 2000,
+reset: true
 })
 
 srLeft.reveal('.about-info',{delay: 100})
@@ -103,10 +111,10 @@ srLeft.reveal('.contact-info',{delay: 100})
 
 /* -- ABOUT SKILLS & FORM BOX -- */
 const srRight = ScrollReveal({
-  origin: 'right',
-  distance: '80px',
-  duration: 2000,
-  reset: true
+origin: 'right',
+distance: '80px',
+duration: 2000,
+reset: true
 })
 
 srRight.reveal('.skills-box',{delay: 100})
@@ -117,23 +125,23 @@ srRight.reveal('.form-control',{delay: 100})
 const sections = document.querySelectorAll('section[id]')
 
 function scrollActive() {
-  const scrollY = window.scrollY;
+const scrollY = window.scrollY;
 
-  sections.forEach(current =>{
-    const sectionHeight = current.offsetHeight,
-        sectionTop = current.offsetTop - 50,
-      sectionId = current.getAttribute('id')
+sections.forEach(current =>{
+  const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 50,
+    sectionId = current.getAttribute('id')
 
-    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) { 
+  if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) { 
 
-        document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
 
-    }  else {
+  }  else {
 
-      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
 
-    }
-  })
+  }
+})
 }
 
 window.addEventListener('scroll', scrollActive)
