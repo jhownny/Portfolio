c82919442b29345a8315e495d078235ebc253b2c
Jhonata Batista <88687538+jhownny@users.noreply.github.com>
2023-09-28 04:34:27 -0300

    Custom - 28/09 Desktop

---
+++ script/script_404.js
@@ -0,0 +1,8 @@
+/* ----- TYPING EFFECT ----- */
+let typingEffect = new Typed(".typedText_404",{
+    strings : ['Parece que não tem nada por aqui...','Quem sabe mais tarde!'],
+    loop : true,
+    typeSpeed : 100, 
+    backSpeed : 100,
+    backDelay : 2000
+  })
